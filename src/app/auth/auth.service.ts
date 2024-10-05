9eeb21100d18971fdf226606a2db4657bd4d1cb0
Petar Gutov <pgutov@gmail.com>
2024-10-05 13:39:52 +0200

    current theme view created

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
 
+import { AuthUser } from '../core/models/auth-user';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  user: AuthUser | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isLoggedIn(): boolean {
+    /* !! cast to boolean. If null or undefined retuns false. If object or array returns true */
+    
+    return !!this.user;
+  }
+
+  constructor() {
+    const localStorageUser = localStorage.getItem(this.USER_KEY) || '';
+    this.user = JSON.parse(localStorageUser);
+  }
+  login() {
+    this.user = {
+      username: 'Petko',
+      email: 'blabla@gmail.com',
+
+      phoneNumber: '123456',
+      password: 'password',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
